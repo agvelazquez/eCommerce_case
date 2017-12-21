fa4776aae26a7b74507ecda6c03aa7e3376a7579
Agustin <30300165+agvelazquez@users.noreply.github.com>
2017-12-21 13:26:17 +0100

    Add files via upload

--- olx_product_strategy.py
+++ olx_product_strategy.py
@@ -16,12 +16,11 @@ category = pd.read_csv('category_data.csv')
 item = pd.read_csv('item_data.csv')
 
 #%%%%% DATA PREPROCESSING %%%%%%%
-os.chdir('D:\Sistema\Santiago\Desktop\OLX')
-category = pd.read_csv('category_data.csv')
-item = pd.read_csv('item_data.csv')
+
 item['first_reply_date'] = pd.to_datetime(item['first_reply_date'])
 item['last_reply_date'] = pd.to_datetime(item['last_reply_date'])
 item['date_live_nk'] = pd.to_datetime(item['date_live_nk'])
+
 categories_item = item['category_sk'].str.split('|',expand = True)
 item = pd.concat([item,categories_item[3],categories_item[4]], axis = 1)
 item = item.rename(columns = {3:'l1_category',4:'l2_category'})
@@ -39,56 +38,56 @@ l2_category = pd.concat([l2_category, category['category_l2_name_en']], axis = 1
 
 
 #%%%%%%%% PLOTS %%%%%%%%%
-    #%% Rate of replies given by the seller    
-    item2 = item[item.first_reply_date < '2017-09-17'] #date considerer to give a margin to the seller
-    
-    p = item2['successful_replies'] / item2['replies']
-    
-    fig = plt.figure()
-    ax1 = fig.add_subplot(111)
-    p.plot(kind = 'hist')
-    ax1.set_xlabel('Succesfull_replies/Replies')
-    ax1.set_ylabel('Items')
-    #plt.yticks(y,item['listing_sk'])
-    #%% Items antique
-    x1 = pd.to_datetime(item['date_live_nk']) 
-    x2 =  pd.to_datetime(item['last_reply_date'][3]) #Last day of analysis 21-09-2017
-    ia = (x2 - x1).dt.days
-    
-    fig2 = plt.figure()
-    ax2 = fig2.add_subplot(111)
-    x.plot(kind = 'hist')
-    ax2.set_xlabel('Item Antique (in days)')
-    ax2.set_ylabel('Items')
-    ax2.set_title('Histogram : Intem Antiquity')
-    #%% Items per user
-    fig3 = plt.figure()
-    ax3 = fig3.add_subplot(111)
-    item['user_sk_encrypted'].value_counts().plot(kind = 'hist')
-    ax3.set_xlabel('Number of items per user')
-    ax3.set_ylabel('Items')
-    ax3.set_title('Items per Seller') 
-    #%% Conversation relevance
-    fig4 = plt.figure()
-    ax4 = fig4.add_subplot(111)
-    item['average_conversation_length'].plot(kind = 'hist')
-    ax4.set_xlabel('Average conversation length')
-    ax4.set_ylabel('Items')
-    ax4.set_title('Histogram : Average consertaion length per item')   
-    #%% Intensity of replies per item
-    # i = replies / item antique
-    i = item[item.date_live_nk < '2017-09-21']['replies'] / ia[ia > 0]
-    fig5 = plt.figure()
-    ax5 = fig5.add_subplot(111)
-    i.plot(kind = 'hist', bins = 20)
-    ax5.set_xlabel('intensity = replies / item antiquity')  
-    ax5.set_ylabel('Items')
-    ax5.set_title('Histogram : Intensity of replies per item')  
-    #%% Histogram of categories
-    plt.figure()
-    f = sns.countplot(y='l1_category', data=item)
-    f.set(yticklabels = l1_category['category_l1_name_en'])
-    f.figure.tight_layout()
+#%% Rate of replies given by the seller    
+item2 = item[item.first_reply_date < '2017-09-17'] #date considerer to give a margin to the selle
+p = item2['successful_replies'] / item2['replies']
+
+fig = plt.figure()
+ax1 = fig.add_subplot(111)
+p.plot(kind = 'hist')
+ax1.set_xlabel('Succesfull_replies/Replies')
+ax1.set_ylabel('Items')
+#plt.yticks(y,item['listing_sk'])
+#%% Items antique
+x1 = pd.to_datetime(item['date_live_nk']) 
+x2 =  pd.to_datetime(item['last_reply_date'][3]) #Last day of analysis 21-09-2017
+ia = (x2 - x1).dt.days
+
+fig2 = plt.figure()
+ax2 = fig2.add_subplot(111)
+ia.plot(kind = 'hist')
+ax2.set_xlabel('Item Antique (in days)')
+ax2.set_ylabel('Items')
+ax2.set_title('Histogram : Intem Antiquity')
+#%% Items per user
+is_1 = item['user_sk_encrypted'].value_counts()
+fig3 = plt.figure()
+ax3 = fig3.add_subplot(111)
+item['user_sk_encrypted'].value_counts().plot(kind = 'hist')
+ax3.set_xlabel('Number of items per user')
+ax3.set_ylabel('Items')
+ax3.set_title('Items per Seller') 
+#%% Conversation relevance
+fig4 = plt.figure()
+ax4 = fig4.add_subplot(111)
+item['average_conversation_length'].plot(kind = 'hist')
+ax4.set_xlabel('Average conversation length')
+ax4.set_ylabel('Items')
+ax4.set_title('Histogram : Average consertaion length per item')   
+#%% Intensity of replies per item
+# i = replies / item antique
+i = item[item.date_live_nk < '2017-09-21']['replies'] / ia[ia > 0]
+fig5 = plt.figure()
+ax5 = fig5.add_subplot(111)
+i.plot(kind = 'hist', bins = 20)
+ax5.set_xlabel('intensity = replies / item antiquity')  
+ax5.set_ylabel('Items')
+ax5.set_title('Histogram : Intensity of replies per item')  
+#%% Histogram of categories
+plt.figure()
+f = sns.countplot(y='l1_category', data=item)
+f.set(yticklabels = l1_category['category_l1_name_en'])
+f.figure.tight_layout()
 #%% Ranking in one category
 #example in category 16
 ia = (x2 - x1).dt.days
